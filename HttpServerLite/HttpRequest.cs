2886cc1ed4b60e8d5e99dd5a09c980ced192938f
Joel Christner <joel.christner@gmail.com>
2020-08-30 09:59:18 -0700

    NuGet v1.0.1.1, APIs to determine if header or querystring key exists

--- HttpServerLite/HttpRequest.cs
+++ HttpServerLite/HttpRequest.cs
@@ -290,7 +290,65 @@ namespace HttpServerLite
 
             return null;
         }
-         
+
+        /// <summary>
+        /// Determine if a header exists.
+        /// </summary>
+        /// <param name="key">Header key.</param>
+        /// <param name="caseSensitive">Specify whether a case sensitive search should be used.</param>
+        /// <returns>True if exists.</returns>
+        public bool HeaderExists(string key, bool caseSensitive)
+        {
+            if (String.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
+
+            if (caseSensitive)
+            {
+                return Headers.ContainsKey(key);
+            }
+            else
+            {
+                if (Headers != null && Headers.Count > 0)
+                {
+                    foreach (KeyValuePair<string, string> header in Headers)
+                    {
+                        if (String.IsNullOrEmpty(header.Key)) continue;
+                        if (header.Key.ToLower().Trim().Equals(key)) return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Determine if a querystring entry exists.
+        /// </summary>
+        /// <param name="key">Querystring key.</param>
+        /// <param name="caseSensitive">Specify whether a case sensitive search should be used.</param>
+        /// <returns>True if exists.</returns>
+        public bool QuerystringExists(string key, bool caseSensitive)
+        {
+            if (String.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
+
+            if (caseSensitive)
+            {
+                return QuerystringEntries.ContainsKey(key);
+            }
+            else
+            {
+                if (QuerystringEntries != null && QuerystringEntries.Count > 0)
+                {
+                    foreach (KeyValuePair<string, string> queryElement in QuerystringEntries)
+                    {
+                        if (String.IsNullOrEmpty(queryElement.Key)) continue;
+                        if (queryElement.Key.ToLower().Trim().Equals(key)) return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
         /// <summary>
         /// Read the data stream fully and retrieve the string data contained within.
         /// Note: if you use this method, you will not be able to read from the data stream afterward.
